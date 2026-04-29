f12e40588cd6bd06193c326825bbfd8aa6f00e0c
Sonya Novikova <sona.nn@yandex.ru>
2026-04-29 23:58:08 +0300

    Added text about camera init

--- widgets/photo_page.py
+++ widgets/photo_page.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel
 from PyQt6.QtCore import Qt
 from widgets.photo_frame import PhotoFrame
 from constants import MAX_PADDING, MIN_PADDING
@@ -7,16 +7,24 @@ class PhotoPage(QWidget):
     def __init__(self):
         super().__init__()
         self.__photo_frame = PhotoFrame()
+        self.__wait_photo_label = QLabel("Настройкка камеры. Пожалуйста, подождите...")
+        self.__layout = None
         self.__create_main_layout()
     
     def __create_main_layout(self):    
-        layout = QVBoxLayout()
-        layout.setContentsMargins(MIN_PADDING, MAX_PADDING, MIN_PADDING, MAX_PADDING)
-        layout.setSpacing(MAX_PADDING)
-        layout.setAlignment(Qt.AlignmentFlag.AlignTop)
-        layout.addWidget(self.__photo_frame, 0, Qt.AlignmentFlag.AlignHCenter)
-        self.setLayout(layout)
+        self.__layout = QVBoxLayout()
+        self.__layout.setContentsMargins(MIN_PADDING, MAX_PADDING, MIN_PADDING, MAX_PADDING)
+        self.__layout.setSpacing(MAX_PADDING)
+        self.__layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+        self.__layout.addWidget(self.__wait_photo_label, 0, Qt.AlignmentFlag.AlignHCenter)
+        self.setLayout(self.__layout)
         
     def set_photo(self, photo_path):
+        if self.__wait_photo_label and self.__layout:
+            if self.__layout.indexOf(self.__wait_photo_label) != -1:
+                self.__layout.replaceWidget(self.__wait_photo_label, self.__photo_frame)
+                self.__wait_photo_label.hide()
+                self.__wait_photo_label = None
+
         self.__photo_frame.set_photo(photo_path)
         
