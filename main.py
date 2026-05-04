2be38f997bc9800a4c393eef9eb5ce9be93f7f7c
Sonya Novikova <sona.nn@yandex.ru>
2026-05-04 12:52:11 +0300

    Added an app icon

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QIcon
 from main_window import MainWindow
 
 app = QApplication(sys.argv)
+app.setWindowIcon(QIcon("icons/app_icon.png"))
 main_window = MainWindow()
 main_window.show()
 sys.exit(app.exec())
