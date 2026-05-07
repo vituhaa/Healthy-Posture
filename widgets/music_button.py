69b1eb4da58030f9572345221b2e94e636095296
Sonya Novikova <sona.nn@yandex.ru>
2026-05-08 00:02:52 +0300

    Created music buttons

---
+++ widgets/music_button.py
@@ -0,0 +1,28 @@
+from PyQt6.QtWidgets import QRadioButton
+from PyQt6.QtMultimedia import QMediaPlayer, QAudioOutput
+from PyQt6.QtCore import QUrl, QFile
+
+class MusicButton(QRadioButton):
+    def __init__(self, path_to_music_file):
+        super().__init__()
+        
+        self.__file = QFile(path_to_music_file)
+        if self.__file.exists:
+            self.setText(self.__get_file_name(path_to_music_file))
+            self.__audio_output = QAudioOutput()
+            self.__audio_output.setVolume(1)
+            self.__music = QMediaPlayer()
+            self.__music.setAudioOutput(self.__audio_output)
+            self.__music.setSource(QUrl.fromLocalFile(path_to_music_file))
+            self.clicked.connect(lambda: (self.__music.play(),
+                                        self.setChecked(True)))
+        
+    def __get_file_name(self, file):
+        start_index = 0
+        if '/' in file:
+            start_index = file.rfind('/') + 1
+        elif '\\' in file:
+            start_index = file.rfind('\\') + 1
+        end_index = file.rfind('.')
+        return file[start_index:end_index]
+        
