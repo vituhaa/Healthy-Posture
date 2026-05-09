8787ab6965efce231ca86276e1471588301a7b60
Sonya Novikova <sona.nn@yandex.ru>
2026-05-10 01:22:35 +0300

    Added apply and cancel settings

--- widgets/settings_page.py
+++ widgets/settings_page.py
@@ -19,6 +19,16 @@ class SettingsPage(QWidget):
         self.__preventive_notification_group = None
         self.__apply_button = None
         self.__cancel_button = None
+        
+        self.__settings = {"posture_analysis_frequency": 0 # in seconds
+                           , "is_posture_notifications_on": True
+                           , "is_posture_music_on": True
+                           , "posture_music": "" # path to music file
+                           , "is_preventive_notifications_on": True
+                           , "preventive_notification_frequency": 0 # in seconds
+                           , "is_preventive_music_on": True
+                           , "preventive_music": ""} # path to music file
+        
         self.__create_main_layout()
         
     def __create_main_layout(self):
@@ -65,6 +75,8 @@ class SettingsPage(QWidget):
         self.__posture_analysis_frequency.set_interval("0:0:5", "0:10:0")
         self.__posture_analysis_frequency.set_value("0:0:5")
         layout.addWidget(self.__posture_analysis_frequency, 0, Qt.AlignmentFlag.AlignLeft)
+        self.__settings["posture_analysis_frequency"] = 5
+        self.__posture_analysis_frequency.time_changed.connect(lambda time: self.__settings_changed())
         
         # notification settings
         layout.addWidget(notification_label, 0, Qt.AlignmentFlag.AlignLeft)
@@ -90,10 +102,14 @@ class SettingsPage(QWidget):
         buttons_layout.setSpacing(MAX_PADDING)
         
         self.__apply_button = QPushButton("Применить")
+        self.__apply_button.setEnabled(False)
         buttons_layout.addWidget(self.__apply_button)
+        self.__apply_button.clicked.connect(self.__settings_changes_apply)
         
         self.__cancel_button = QPushButton("Отменить")
+        self.__cancel_button.setEnabled(False)
         buttons_layout.addWidget(self.__cancel_button)
+        self.__cancel_button.clicked.connect(self.__settings_changes_cancel)
         
         layout.addLayout(buttons_layout, 0)
         
@@ -126,6 +142,8 @@ class SettingsPage(QWidget):
         self.__posture_music_widget = MusicGroup()
         self.__create_music_choice(self.__posture_music_widget)
         layout.addWidget(self.__posture_music_widget, 0, Qt.AlignmentFlag.AlignLeft)
+        self.__settings["posture_music"] = self.__posture_music_widget.get_current_track()
+        self.__posture_music_widget.music_chose.connect(lambda music: self.__settings_changed())
         
         widget.setLayout(layout)
         
@@ -149,6 +167,8 @@ class SettingsPage(QWidget):
         self.__preventive_notification_frequency.set_interval("0:15:0", "2:0:0")
         self.__preventive_notification_frequency.set_value("0:15:0")
         layout.addWidget(self.__preventive_notification_frequency, 0, Qt.AlignmentFlag.AlignLeft)
+        self.__settings["preventive_notification_frequency"] = 15 * 60
+        self.__preventive_notification_frequency.time_changed.connect(lambda time: self.__settings_changed())
         
         audio_label = QLabel("Звук")
         audio_label.setFont(text_font)
@@ -157,14 +177,52 @@ class SettingsPage(QWidget):
         self.__preventive_music_widget = MusicGroup()
         self.__create_music_choice(self.__preventive_music_widget)
         layout.addWidget(self.__preventive_music_widget, 0, Qt.AlignmentFlag.AlignLeft)
+        self.__settings["preventive_music"] = self.__preventive_music_widget.get_current_track()
+        self.__preventive_music_widget.music_chose.connect(lambda music: self.__settings_changed())
         
         widget.setLayout(layout)
         
         return widget
+    
+    def __settings_changed(self):
+        if not self.__apply_button.isEnabled():
+            self.__apply_button.setEnabled(True)
+            self.__cancel_button.setEnabled(True)
+            
+    def __settings_changes_cancel(self):
+        posture_total_seconds = self.__settings["posture_analysis_frequency"]
+        if self.__posture_analysis_frequency.get_total_seconds() != posture_total_seconds:
+            self.__posture_analysis_frequency.set_total_seconds_value(posture_total_seconds)
+            
+        preventive_total_seconds = self.__settings["preventive_notification_frequency"]
+        if self.__preventive_notification_frequency.get_total_seconds() != preventive_total_seconds:
+            self.__preventive_notification_frequency.set_total_seconds_value(preventive_total_seconds)
+        posture_track = self.__settings["posture_music"]
+        if self.__posture_music_widget.get_current_track() != posture_track:
+            self.__posture_music_widget.set_track(posture_track)
+        preventive_track = self.__settings["preventive_music"]
+        if self.__preventive_music_widget.get_current_track() != preventive_track:
+            self.__preventive_music_widget.set_track(preventive_track)
+        self.__cancel_button.setEnabled(False)
+        self.__apply_button.setEnabled(False)
         
         
-        
-        
+    def __settings_changes_apply(self):
+        posture_analysis_new = self.__posture_analysis_frequency.get_total_seconds()
+        if posture_analysis_new != self.__settings["posture_analysis_frequency"]:
+            self.__settings["posture_analysis_frequency"] = posture_analysis_new
+            
+        preventive_frequency_new = self.__preventive_notification_frequency.get_total_seconds()
+        if  preventive_frequency_new != self.__settings["preventive_notification_frequency"]:
+            self.__settings["preventive_notification_frequency"] = preventive_frequency_new
+        posture_track_new = self.__posture_music_widget.get_current_track()
+        if posture_track_new != self.__settings["posture_music"]:
+            self.__settings["posture_music"] = posture_track_new
+        preventive_track_new = self.__preventive_music_widget.get_current_track()
+        if preventive_track_new != self.__settings["preventive_music"]:
+            self.__settings["preventive_music"] = preventive_track_new
+        self.__cancel_button.setEnabled(False)
+        self.__apply_button.setEnabled(False)
         
         
         
